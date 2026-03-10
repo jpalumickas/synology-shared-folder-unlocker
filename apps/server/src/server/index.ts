9f91d51b3ea468892cd479949edca2fea8ca762a
Justas Palumickas <jpalumickas@gmail.com>
2026-03-10 03:22:04 +0200

    Add sigint/sigterm

--- apps/server/src/server/index.ts
+++ apps/server/src/server/index.ts
@@ -1,9 +1,21 @@
 import { serve } from '@hono/node-server'
+import { stopPoller } from '@synology-shared-folder-unlocker/unlocker'
 import { app } from './app.ts'
 
 const port = parseInt(process.env.PORT || '3001', 10)
 const hostname = process.env.HOST || '127.0.0.1'
 
-serve({ fetch: app.fetch, port, hostname }, (info) => {
+const server = serve({ fetch: app.fetch, port, hostname }, (info) => {
   console.log(`Server running at http://${info.address}:${info.port}`)
 })
+
+function shutdown() {
+  console.log('Shutting down...')
+  stopPoller()
+  server.close(() => {
+    process.exit(0)
+  })
+}
+
+process.on('SIGINT', shutdown)
+process.on('SIGTERM', shutdown)
